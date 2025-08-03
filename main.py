72464dd61ad104de04feb3f8e55ec60df550537e
zoekhall <zoe.k.hall@gmail.com>
2025-08-03 01:42:43 -0500

    add spaces for input/print

--- main.py
+++ main.py
@@ -21,24 +21,24 @@ def get_yes_no(prompt):
     response = input(prompt).strip().upper()
     if response in (*VALID_YES, *VALID_NO):
       return response
-    print("Please enter Yes or No.")
+    print("Please enter Yes or No. ")
 
 def get_name(taken_name=None):
   while True:
-    name = input("What would you like your name to be?").strip() 
+    name = input("What would you like your name to be? ").strip() 
     if name != taken_name:    
       return name
-    print(f"That name has already been chosen. Please pick a unique name.")
+    print(f"That name has already been chosen. Please pick a unique name. ")
       
 def get_token(taken_token=None):
   while True:
-    token = input("What would you like your token to be? Please pick one character that is not a number.").strip()
+    token = input("What would you like your token to be? Please pick one character that is not a number. ").strip()
     if len(token) != 1:
-      print("Please enter a single character.")
+      print("Please enter a single character. ")
     elif token.isdigit(): 
-      print("Please don't use a number.")
+      print("Please don't use a number. ")
     elif token == taken_token:
-      print("That token is already taken. Try another one.")     
+      print("That token is already taken. Try another one. ")     
     else:
       return token 
 
@@ -47,24 +47,24 @@ def get_number(prompt, min_val, max_val):
     val = input(prompt).strip()
     if val.isdigit() and min_val <=int(val) <= max_val: 
       return int(val)      
-    print(f"Please pick a valid number between {min_val} and {max_val}.")
+    print(f"Please pick a valid number between {min_val} and {max_val}. ")
 
 # --------------------------- Game Setup Functions --------------------------- #
 def setup_row_or_col(type):
-  response = get_yes_no(f"Would you like to change the # of {type['name']}? (default = {type['default']})")
+  response = get_yes_no(f"Would you like to change the # of {type['name']}? (default = {type['default']}) ")
   if response in VALID_YES:
-    return get_number(f"How many {type['name']} would you like? (min = {type['min']}, max = {type['max']})", type['min'], type['max'])  
+    return get_number(f"How many {type['name']} would you like? (min = {type['min']}, max = {type['max']}) ", type['min'], type['max'])  
   else:
-    print(f"Okay! We'll set the # of {type['name']} to {type['default']}.")
+    print(f"Okay! We'll set the # of {type['name']} to {type['default']}. ")
     return type['default']  
   
 def setup_player_name(player_num, other_name=None):
-  player_name = get_name(other_name) if get_yes_no("Would you like to choose your name?") in VALID_YES else player_num
-  print(f"Great! You've chosen the name: {player_name}.")
+  player_name = get_name(other_name) if get_yes_no("Would you like to choose your name? ") in VALID_YES else player_num
+  print(f"Great! You've chosen the name: {player_name}. ")
   return player_name
 
 def setup_player_token(default_token, other_token=None):
-  response = get_yes_no("Would you like to choose your token?") #ask if want to choose token
+  response = get_yes_no("Would you like to choose your token? ") #ask if want to choose token
   if response in VALID_YES:
     token = get_token(other_token)
   else:
@@ -72,7 +72,7 @@ def setup_player_token(default_token, other_token=None):
       token = 'O' if default_token == 'X' else 'X'
     else:
       token = default_token   
-  print(f"Great! You've chosen the token: {token}.")
+  print(f"Great! You've chosen the token: {token}. ")
   return token
 
 def setup_player(name, default_token, other_name=None, other_token=None):
@@ -104,12 +104,12 @@ class ConnectFour:
   #ask for/handle player's input
   def get_player_input(self): 
     while True:
-      colStr = input(f"{self.current_player.name}, where would you like to place your token? (choose a column #)")
+      colStr = input(f"{self.current_player.name}, where would you like to place your token? (choose a column #) ")
       if colStr.isdigit():
         colNum = int(colStr)
         if 1 <= colNum <= self.cols:
           return colNum - 1
-      print(f"Please enter a number between 1-{self.cols}")
+      print(f"Please enter a number between 1-{self.cols}. ")
 
   #add token to the board
   def drop_token(self, col):
@@ -148,7 +148,7 @@ class ConnectFour:
       col = self.get_player_input() #pick column #
       row = self.drop_token(col) #drop token into column 
       if row == False:
-        print("Whoops! That column is full. Please pick another column.")
+        print("Whoops! That column is full. Please pick another column. ")
         continue 
       
       self.detect_win(row, col)
@@ -169,7 +169,7 @@ player_2 = setup_player('Player 2', 'O', other_name = player_1.name, other_token
 
 # ------------------------------- Board Set-Up ------------------------------- #
 print("Alright! Let's set up your board.")
-response = get_yes_no("Would you like to customize your board?") #ask if want to customize board
+response = get_yes_no("Would you like to customize your board? ") #ask if want to customize board
 if response in VALID_YES:
   rows, cols = setup_row_or_col(ROWS), setup_row_or_col(COLS)  
 else:
